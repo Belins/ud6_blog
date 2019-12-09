3a0e726ecdb9c309a8a7631b15c8869aa0779666
Belins <dbelinchonal18dw@ikzubirimanteo.com>
2019-12-09 13:16:13 +0100

    foto actualizada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,8 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Vuestro nombre</a>
+      <img src="{{asset('assets/img/foto.png')}}" width="70" height="50" style="margin-right: 20px;">
+      <a class="navbar-brand" href="#">David Belinchon</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
